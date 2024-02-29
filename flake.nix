baa87e2762b5aff23cdf02d381240d9056f3c4cc
Milo Moisson <milomoisson@gmail.com>
2024-02-29 16:35:16 +0100

    chore: add nix stuff

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, gitignore }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      libraries = [ ];
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        rustToolchain
+        rust-analyzer
+        act
+      ];
+      buildInputs = [ ];
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+
+      packages = rec {
+        default = dapi;
+        dapi = pkgs.callPackage ./package.nix { inherit gitignore; };
+      };
+      apps = rec {
+        default = dapi;
+        dapi = flake-utils.lib.mkApp { drv = self.packages.${system}.dapi; };
+      };
+
+      devShells.default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs;
+
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
+
+        RUST_LOG="";
+      };
+    }
+  );
+}
